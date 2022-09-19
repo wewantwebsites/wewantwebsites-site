bc64fcacb37bf167968161ab1a123a04cba51b70
wewantwebsites <cas.guzdziol@wewantweb.site>
2022-09-19 16:19:13 -0400

    started a sveltekit site

---
+++ site/src/lib/header/Header.svelte
@@ -0,0 +1,112 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+
+	const routes = [
+		{
+			label: 'Home',
+			target: '/'
+		},
+		{
+			label: 'About',
+			target: '/about'
+		},
+		{
+			label: 'Services',
+			target: '/services'
+		},
+		{
+			label: 'Contact',
+			target: '/contact'
+		}
+	];
+</script>
+
+<header>
+	<nav data-sveltekit-prefetch>
+		<svg viewBox="0 0 2 3" aria-hidden="true">
+			<path d="M0,0 L1,2 C1.5,3 1.5,3 2,3 L2,0 Z" />
+		</svg>
+		<ul>
+			<li>
+				<a href="/">
+					<img width="100" src="/logo.png" alt="We Want Web Logo" />
+				</a>
+			</li>
+			{#each routes as { label, target }}
+				<li class:active={$page.url.pathname === target}><a href={target}>{label}</a></li>
+			{/each}
+		</ul>
+		<svg viewBox="0 0 2 3" aria-hidden="true">
+			<path d="M0,0 L0,3 C0.5,3 0.5,3 1,2 L2,0 Z" />
+		</svg>
+	</nav>
+</header>
+
+<style>
+	header {
+		display: flex;
+		justify-content: center;
+	}
+
+	nav {
+		display: flex;
+		justify-content: center;
+	}
+
+	svg {
+		width: 2em;
+		height: 3em;
+		display: block;
+	}
+
+	path {
+		fill: var(--bg);
+	}
+
+	ul {
+		position: relative;
+		padding: 0;
+		margin: 0;
+		height: 3em;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		list-style: none;
+		background: var(--bg);
+		background-size: contain;
+	}
+
+	li {
+		position: relative;
+		height: 100%;
+	}
+
+	li.active::before {
+		--size: 6px;
+		content: '';
+		width: 0;
+		height: 0;
+		position: absolute;
+		top: 0;
+		left: calc(50% - var(--size));
+		border: var(--size) solid transparent;
+		border-top: var(--size) solid var(--accent);
+	}
+
+	nav a {
+		display: flex;
+		height: 100%;
+		align-items: center;
+		padding: 0 1em;
+		font-weight: 700;
+		font-size: 0.8rem;
+		text-transform: uppercase;
+		letter-spacing: 0.1em;
+		text-decoration: none;
+		transition: color 0.2s linear;
+	}
+
+	a:hover {
+		color: var(--accent);
+	}
+</style>
---
+++ site/src/routes/about/+page.svelte
@@ -0,0 +1,18 @@
+<svelte:head>
+	<title>About | We Want Web LLC</title>
+	<meta name="description" content="About this app" />
+</svelte:head>
+
+<div class="content">
+	<h1>About: <span>We Want Web</span></h1>
+
+	<p>Some content about us</p>
+</div>
+
+<style>
+	.content {
+		width: 100%;
+		max-width: var(--column-width);
+		margin: var(--column-margin-top) auto 0 auto;
+	}
+</style>
